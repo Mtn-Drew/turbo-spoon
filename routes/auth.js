df2d2beeb72effaf82b679ba9ce776185d152abe
Mtn-Drew <drewc3@gmail.com>
2022-01-20 15:09:39 -0700

    Add login

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,6 @@ const { check, validationResult } = require("express-validator")
 const { users } = require("../db")
 const bcrypt = require("bcrypt")
 const JWT = require("jsonwebtoken")
-
 router.post(
 	"/signup",
 	[
@@ -21,9 +20,7 @@ router.post(
 				errors: errors.array(),
 			})
 		}
-
 		// Validate if user doesn't already exist
-
 		let user = users.find((user) => {
 			return user.email === email
 		})
@@ -56,12 +53,52 @@ router.post(
 			}
 		)
 
-    res.json({token})
-
-		console.log("PW: ", hashedPassword)
-		res.send("Validation Passed")
+		res.json({ token })
 	}
 )
+
+router.post("/login", async (req, res) => {
+	const { password, email } = req.body
+
+	let user = users.find((user) => {
+		return user.email === email
+	})
+
+	if (!user) {
+		return res.status(400).json({
+			errors: [
+				{
+					msg: "Invalid Credentials",
+				},
+			],
+		})
+	}
+
+	let isMatch = await bcrypt.compare(password, user.password)
+
+	if (!isMatch) {
+		return res.status(400).json({
+			errors: [
+				{
+					msg: "Invalid Credentials",
+				},
+			],
+		})
+	}
+	const token = await JWT.sign(
+		{
+			//this will need to be improved for security
+			email,
+		},
+		"0innu8enfe8unmknwdbnwd",
+		{
+			//this should be in an env file
+			expiresIn: 3600000,
+		}
+	)
+
+	res.json({ token })
+})
 
 router.get("/all", (req, res) => {
 	res.json(users)
